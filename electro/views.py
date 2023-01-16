8b0359869e6c57910367538c60308f73426e9511
Tech <soran.hamad@epu.edu.iq>
2023-01-16 15:55:50 +0300

    new

--- electro/views.py
+++ electro/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from electro.models import *
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'electro/index.html')
+
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('electro/index.html')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                messages.info(request, 'Username Or Password is Incorrect')
+                return render(request, 'electro/login.html')
+        context = {}
+        return render(request, 'electro/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
--- electrobazar/settings.py
+++ electrobazar/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -104,16 +104,27 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = 'Asia/Baghdad'
 
 USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
